138792672372dc307e23ef825eddc139a90222ad
VivianRelvas <vivianrelvas@hotmail.com>
2021-08-28 14:27:56 +0000

    declarando a anotação

--- src/main/java/application/controllers/HomeController.java
+++ src/main/java/application/controllers/HomeController.java
@@ -1,13 +1,14 @@
 package application.controllers;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Controller;   //importando 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Controller
+
+@Controller    //@ é uma anotação de uma classe
 public class HomeController{ 
-     @RequestMapping(method=RequestMethod.get)
+     @RequestMapping(method=RequestMethod.GET) //@ é uma anotação de uma classe com parâmetro 
 
       public String index(){
       return "home/index.jsp";
