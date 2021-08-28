b28a5d0071f88ef40188aae10c3a2abbe15e0804
VivianRelvas <vivianrelvas@hotmail.com>
2021-08-28 02:19:45 +0000

    Aplicação concluida

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -2,6 +2,8 @@ package application;
 public class Application{
     public static void main (String args[]){
      
+     SpringApplication.run(Application.class, args);
+     
      //git add .
      //git commit -m "Criando os pacotes" subir o arquivo
      //git push - atualizar o arquivo
