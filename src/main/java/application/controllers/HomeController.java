1422c4bc0d5c17b69b1d870bdae9bb1acfcca6c9
VivianRelvas <vivianrelvas@hotmail.com>
2021-08-28 02:23:59 +0000

    atualização

--- src/main/java/application/controllers/HomeController.java
+++ src/main/java/application/controllers/HomeController.java
@@ -3,6 +3,7 @@ package application.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @Controller
 public class HomeController{
