1e60c9dd688045d0301433a8939f085f4c304330
VivianRelvas <vivianrelvas@hotmail.com>
2021-08-31 19:02:31 +0000

    Atualizando arquivos

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -3,7 +3,7 @@ package application;  //importando o pacote
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication; // importanto as classes
 
-
+@SpringBootApplication
 public class Application {
     public static void main(String args[]){
      
--- src/main/webapp/home/index.jsp
+++ src/main/webapp/home/index.jsp
@@ -1,7 +1,6 @@
 //charset para não ter problemas com acentuação 
 
-<%@ page language="java" contentType="text/html"; charset="UTF-8"  
-pageEncoding="UTF-8"%>
+<%@ page language="java" contentType="type/html"; charset="UTF-8" pageEncoding="UTF-8"%>
 
 <!DOCTYPE html>
 
